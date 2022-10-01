2d7c6ea39628735fe6d1bda16b744fab3568fbc4
Simon Pitre-Lamas <pitresimon@gmail.com>
2022-10-01 15:09:37 -0400

    Ajout des methodes

--- src/TP1/UtilitaireTableauCartes.java
+++ src/TP1/UtilitaireTableauCartes.java
@@ -4,6 +4,13 @@ public class UtilitaireTableauCartes {
 
 	public UtilitaireTableauCartes() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	public static Carte[] copieDuJeu(Carte[] jeuneuf) {
+		return jeuneuf;
+	}
+	public static Boolean toutesLesCartesSontTournee(Carte[] cartesAffichees) {
+		return true;
 	}
 
 }
