0d08d09960415137e8c51ddfd348a97636978dc4
rchatterjee007 <90978654+rchatterjee007@users.noreply.github.com>
2022-10-02 19:28:30 -0400

    Update UtilitaireTableauCartes.java

--- src/TP1/UtilitaireTableauCartes.java
+++ src/TP1/UtilitaireTableauCartes.java
@@ -84,53 +84,71 @@ public class UtilitaireTableauCartes {
 		}
 		return sousTab;
 	}
-
-	public static void brasser(Carte[] cartes){	
+public static Carte[] separerCartesEn2(Carte [] tabCarteOriginale, int indiceDebut, int indiceFin){
+		Carte[] sousTab = new Carte[indiceFin - indiceDebut + 1];
+		int j = 0;
+		for(int i  = indiceDebut; i <= indiceFin; i++){
+			sousTab[j] = tabCarteOriginale[i];
+			j++;                                              
+		}
+		return sousTab;
+	}
+	public static Carte[] brasser(Carte[] cartes){	
 		// Générer un nombre aléatoire de fois que vous répéterez les trois étapes.
 		int nbrAlea=UtilitaireFonction.alea(NOMBRE_MIN, NOMBRE_MAX);
+		Carte[]nouvTabCarte=new Carte[cartes.length];
 		int i=0;	
-
-
-		// REPETER LE M/LANGE POUR LE X NOMBRE DE FOIS
 		while (i!=nbrAlea){
-
 			melanger(cartes);//melanger les cartes
-
 			//Separer LE PAQUET DE CARTES EN 2
 			int indiceTab1=UtilitaireFonction.alea(20, 30);
 			Carte [] tab1= separerCartesEn2(cartes,0,indiceTab1); 
 			Carte [] tab2= separerCartesEn2(cartes,indiceTab1+1,cartes.length-1); 
-
-			fusionner();
+			nouvTabCarte=fusionner(cartes, tab1, tab2);
 			i++;
 		}
+		return nouvTabCarte;
 	}
-
 	public static void deplacerCartes(Carte [] tab1) {
 		Carte valueBeingMoved = tab1[tab1.length-1];
 		for (int i=tab1.length-1;i>0;i--) {
 			tab1[i] = tab1[i-1];
 		}
 		tab1[0] = valueBeingMoved;
-
-
 	}
-
 	public static void melanger(Carte [] tab) {
 		int nbrFoisMelanger= UtilitaireFonction.alea(NOMBRE_MIN, NOMBRE_MAX);
 		int i=0;
-
+		
 		while (i!=nbrFoisMelanger){
 			int nbrCartesDeplacer=UtilitaireFonction.alea(3, 10);
-			for(i=0;i<nbrCartesDeplacer;i++) {
+			for(int j=0;j<nbrCartesDeplacer;j++) {
 				deplacerCartes(tab);
 			}
 			i++;
 		}	
 	}
-
-	public static void fusionner() {
+	public static Carte[]  fusionner(Carte [] cartesOriginales, Carte [] tab1, Carte [] tab2) {
+		Carte[] tabCartesAfficher= new Carte [cartesOriginales.length];
+		int iterateurTab1=0;
+	    int iterateurTab2=0;
+	    int iteratuerTabCartesFinale=0;
+	    while(iteratuerTabCartesFinale<tabCartesAfficher.length){
+	    	int nbrPairOuImpaire=UtilitaireFonction.alea(0,10);
+	       	if (nbrPairOuImpaire%2==0&&iterateurTab1<tab1.length){
+	       		tabCartesAfficher[iteratuerTabCartesFinale]=tab1[iterateurTab1];
+	       		iterateurTab1++;
+	       		iteratuerTabCartesFinale++;
+	       	}
+	       	if(nbrPairOuImpaire%2==1&&iterateurTab2<tab2.length){
+	       		tabCartesAfficher[iteratuerTabCartesFinale]=tab2[iterateurTab2];
+	       		iterateurTab2++;
+	       		iteratuerTabCartesFinale++;
+	        }
+	    }
+		return tabCartesAfficher;
 	}
+	
 	
 
 }
